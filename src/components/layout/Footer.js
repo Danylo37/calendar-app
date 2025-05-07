d4bc07960c872e7634ba18aa49f3b4d3f1ae0371
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-05-07 18:16:10 +0200

    Footer done

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,12 @@
+import React from 'react';
 import '../../styles/layout/Footer.css';
 
 function Footer() {
-
+    return (
+        <footer className="footer">
+            <span className="copyright">© Danylo Lopatin</span>
+        </footer>
+    );
 }
 
 export default Footer;
