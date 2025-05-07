1cbfc40723f4cb098e239284f17e91b1108c109d
Danylo Lopatin <d.s.lopatin@student.khai.edu>
2025-05-07 18:47:37 +0200

    Icons added for header left buttons

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { PlusCircle, Calendar, ListFilter } from 'lucide-react';
 import '../../styles/layout/Header.css';
 
 function Header() {
     return (
         <header className="header">
             <div className="header-left">
-                <button className="btn">+ Add event</button>
-                <button className="btn">View</button>
-                <button className="btn">Categories</button>
+                <button className="btn">
+                    <PlusCircle size={18} />
+                    <span>Add Event</span>
+                </button>
+                <button className="btn">
+                    <Calendar size={18} />
+                    <span>View</span>
+                </button>
+                <button className="btn">
+                    <ListFilter size={18} />
+                    <span>Categories</span>
+                </button>
             </div>
             <div className="header-right">
                 <button className="btn">&lt;</button>
